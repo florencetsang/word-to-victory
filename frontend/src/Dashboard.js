8e5ecb680116738729cbc7b9376a6ea1c4b3ab35
Florence <florencesytsang@gmail.com>
2024-11-04 18:32:08 +0800

    Fix localStorage, button style.

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BOOKS } from './bible/constants.ts';
-import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import {useEffect, useState} from "react";
 
@@ -13,7 +12,7 @@ function Dashboard() {
         const readStatus = {}
         BOOKS.map(book => {
             for (let i = 1; i < book.numChapters+1; i++) {
-                const chapterKey = book+"_"+i;
+                const chapterKey = book.name+"_"+i;
                 readStatus[chapterKey] = localStorage.getItem(chapterKey) === "true";
             }
         })
@@ -35,37 +34,38 @@ function Dashboard() {
 
     const getButtonVariant = (chapterKey) => {
         let isRead = getReadStatus(chapterKey);
-        return isRead? "solid" : "outlined";
+        return isRead? "contained" : "outlined";
+    };
+
+    const getButtonColor = (chapterKey) => {
+        let isRead = getReadStatus(chapterKey);
+        return isRead? "success" : "neutral";
     };
 
   return (
       <div>
           <p>{readStatus["Genesis_1"]}</p>
           <p>{readStatus["Genesis_2"]}</p>
-          {/*<p>{read}</p>*/}
           {BOOKS && BOOKS.map(book =>
               <div>
                   <p>{book.name}</p>
-                  {/*<p>{book.numChapters}</p>*/}
-                  {book.numChapters && Array.from({length: Math.ceil(book.numChapters / 20)}, (_, i_20) =>
-                      <div>
-                          <ButtonGroup size="small">
-                              {book.numChapters && Array.from({length: Math.min(20, book.numChapters - i_20 * 20)}, (_, i) => {
-                                      const chapterNum = i + 1 + i_20 * 20;
-                                      const chapterKey = book.name + "_" + chapterNum;
-                                      return <Button variant={getButtonVariant(chapterKey)} key={chapterKey}
-                                                     onClick={event => toggleRead(event, chapterKey)}>{chapterNum}</Button>
-                                      // return <Button>{i+1+i_20*20}</Button>
-                                  }
-                              )}
-                          </ButtonGroup>
-                      </div>
-                  )}
-                  {/*<ButtonGroup size="small">*/}
-                  {/*{book.numChapters && Array.from({ length: book.numChapters }, (_, i) =>*/}
-                  {/*    <Button key={i}>{i}</Button>*/}
-                  {/*)}*/}
-                  {/*</ButtonGroup>*/}
+                      {Array.from({length: Math.ceil(book.numChapters)}, (_, i) =>
+                          {
+                              const chapterNum = i + 1;
+                              const chapterKey = book.name + "_" + chapterNum;
+                              return <Button
+                                  key={chapterKey}
+                                  size="small"
+                                  variant={getButtonVariant(chapterKey)}
+                                  color={getButtonColor(chapterKey)}
+                                  disableElevation
+                                  sx={{
+                                      minWidth: 42.81,
+                                  }}
+                                  onClick={event => toggleRead(event, chapterKey)}>{chapterNum}</Button>
+
+                          }
+                      )}
               </div>
           )}
 
